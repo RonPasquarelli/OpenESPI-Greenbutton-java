9efee4ec38273f057fc74661dec952d2e42e0836
Ian Zabel <pair+izabel@pivotallabs.com>
2013-10-10 11:28:30 -0400

    Adds bash-it aliases for Greenbutton project

---
+++ bin/bash/greenbutton.aliases.bash
@@ -0,0 +1,11 @@
+# Aliases for Greenbutton projects.
+# To enable:
+#   ln -s ~/workspace/OpenESPI-Greenbutton-java/bin/bash/greenbutton.aliases.bash ~/.bash_it/aliases/available
+#   bash-it enable alias greenbutton
+
+cite about-alias
+about-alias 'general aliases'
+
+alias gb="cd ~/workspace/OpenESPI-Greenbutton-java"
+alias gbdc="cd ~/workspace/OpenESPI-Greenbutton-java/OpenESPI-DataCustodian-java"
+alias gbtp="cd ~/workspace/OpenESPI-Greenbutton-java/OpenESPI-ThirdParty-java"
