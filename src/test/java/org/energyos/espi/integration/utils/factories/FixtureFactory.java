b7718c957b3bbfe33e8ab6a6772937ffc699b6fc
Josh Clark and William Ramsey <pair+jclark+wramsey@pivotallabs.com>
2013-10-11 14:49:28 -0400

    Fixes compilation errors by loading classpath resources independent of Spring

--- src/test/java/org/energyos/espi/integration/utils/factories/FixtureFactory.java
+++ src/test/java/org/energyos/espi/integration/utils/factories/FixtureFactory.java
@@ -1,11 +1,9 @@
 package org.energyos.espi.integration.utils.factories;
 
-import org.apache.commons.io.FileUtils;
-import org.springframework.core.io.ClassPathResource;
-
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Scanner;
 import java.util.UUID;
 
 public class FixtureFactory {
@@ -16,9 +14,11 @@ public class FixtureFactory {
     }
 
     public static String newUsagePointXML(UUID uuid) throws IOException {
-        ClassPathResource sourceFile = new ClassPathResource("/fixtures/test_usage_data.xml");
-        String xml = FileUtils.readFileToString(sourceFile.getFile());
+        InputStream in = FixtureFactory.class.getClassLoader().getResourceAsStream("fixtures/test_usage_data.xml");
+
+        String xml = new Scanner(in,"UTF-8").useDelimiter("\\A").next();
         xml = xml.replaceFirst("48C2A019-5598-4E16-B0F9-49E4FF27F5FB", uuid.toString());
+
         return xml;
     }
 }
