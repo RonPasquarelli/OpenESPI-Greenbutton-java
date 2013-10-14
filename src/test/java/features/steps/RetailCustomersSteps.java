31b8a27deb751dc9404930a8e6ad89e331922e8c
Brian Kelly and Joshua Clark <pair+bkelly+jclark@pivotallabs.com>
2013-10-14 15:51:37 -0400

    Fixes broken scenarios committed as WIP

--- src/test/java/features/steps/RetailCustomersSteps.java
+++ src/test/java/features/steps/RetailCustomersSteps.java
@@ -48,70 +48,26 @@ public class RetailCustomersSteps {
         driver.get(StepUtils.DATA_CUSTODIAN_BASE_URL + "/logout.do");
     }
 
-    @Then("^I should be logged in$")
-    public void I_should_be_logged_in() throws Throwable {
-        assertTrue(driver.getPageSource().contains("logout"));
-        assertFalse(driver.getPageSource().contains("login"));
-    }
+    @Given("^a Retail Customer with Usage Points$")
+    public void a_Retail_Customer_with_Usage_Points() throws Throwable {
+        CucumberSession.setUsername(StepUtils.newUsername());
 
-    @Then("^I should see the login form$")
-    public void I_should_see_the_login_form() throws Throwable {
-        assertTrue(driver.getPageSource().contains("Login"));
-    }
-
-    @When("^I select Usage Point$")
-    public void I_select_Usage_Point() throws Throwable {
-        WebElement usagePointLink = driver.findElement(By.linkText("Front Electric Meter"));
-        usagePointLink.click();
-    }
-
-    @When("^I select Meter Reading$")
-    public void I_select_Meter_Reading() throws Throwable {
-        WebElement usagePointLink = driver.findElement(By.linkText("Fifteen Minute Electricity Consumption"));
-        usagePointLink.click();
-    }
-
-    @Then("^I should see Meter Reading$")
-    public void I_should_see_Meter_Reading() throws Throwable {
-        assertTrue("MeterReading title missing", driver.getPageSource().contains("Fifteen Minute Electricity Consumption"));
-    }
-
-    @Then("^I should see Reading Type$")
-    public void I_should_see_Reading_Type() throws Throwable {
-        assertTrue("ReadingType title missing", driver.getPageSource().contains("Type of Meter Reading Data"));
-    }
-
-    @Then("^I should see Interval Blocks$")
-    public void I_should_see_Interval_Blocks() throws Throwable {
-        assertTrue("Interval blocks missing", driver.getPageSource().contains("86400"));
-    }
-
-    @Then("^I should see Electric Power Usage Summary$")
-    public void I_should_see_Electric_Power_Usage_Summary() throws Throwable {
-        assertTrue(driver.getPageSource().contains("Usage Summary"));
-    }
-
-    @Then("^I should see Interval Readings$")
-    public void I_should_see_Interval_Readings() throws Throwable {
-        assertTrue(driver.getPageSource().contains("974"));
-        assertTrue(driver.getPageSource().contains("900"));
-        assertTrue(driver.getPageSource().contains("965"));
-    }
-
-    @Then("^I should see Reading Qualities$")
-    public void I_should_see_Reading_Qualities() throws Throwable {
-        WebElement element = driver.findElement(By.cssSelector(".reading-qualities"));
-
-        assertThat(element.getText(), containsString("8"));
-    }
-
-    @Then("^I should see Electric Power Quality Summary$")
-    public void I_should_see_Electric_Power_Quality_Summary() throws Throwable {
-        assertTrue(driver.getPageSource().contains("Quality Summary"));
+        StepUtils.registerUser(CucumberSession.getUsername(), StepUtils.newFirstName(), StepUtils.newLastName(), StepUtils.PASSWORD);
+        CucumberSession.setUUID(UUID.fromString("48C2A019-5598-4E16-B0F9-49E4FF27F5FB"));
+        StepUtils.addUsagePoint(CucumberSession.getUsername(), CucumberSession.getUUID().toString());
+        StepUtils.importUsagePoint();
+        StepUtils.logout();
+        StepUtils.login(StepUtils.THIRD_PARTY_CONTEXT, "alan", StepUtils.PASSWORD);
     }
 
     @Given("^I have a Retail Customer account$")
     public void I_have_a_Retail_Customer_account() throws Throwable {
+        // Seed Data
+    }
+
+    @Given("^There is a Third Party$")
+    public void There_is_a_Third_Party() throws Throwable {
+       // Defined in seed data
     }
 
     @When("^I log in as Alan Turing into Data Custodian$")
@@ -129,21 +85,10 @@ public class RetailCustomersSteps {
         clickLinkByText("Third Parties");
     }
 
-    @Given("^There is a Third Party$")
-    public void There_is_a_Third_Party() throws Throwable {
-       // Defined in seed data
-    }
-
     @When("^I select the Third Party$")
     public void I_select_the_Third_Party() throws Throwable {
         selectRadioByLabel("Pivotal Energy");
         driver.findElement(By.name("next")).click();
-    }
-
-    @Then("^I should be taken to the Third Party login page$")
-    public void I_should_be_taken_to_the_Third_Party_login_page() throws Throwable {
-        assertNotNull("Login field missing", driver.findElement(By.name("j_username")));
-        assertNotNull("Password field missing", driver.findElement(By.name("j_password")));
     }
 
     @When("^I look at my Usage Points page$")
@@ -152,12 +97,50 @@ public class RetailCustomersSteps {
         usagePointLink.click();
 
         StepUtils.submitLoginForm(CucumberSession.getUsername(), StepUtils.PASSWORD);
-        clickByName("authorize");
     }
 
-    @Then("^I should see Usage Point with title \"([^\"]*)\"$")
-    public void I_should_see_my_Usage_Points_with_title(String title) throws Throwable {
-        assertTrue(driver.getPageSource().contains(title));
+    @When("^I select a Data Custodian from the list$")
+    public void I_select_a_Data_Custodian_from_the_list() throws Throwable {
+        clickLinkByText("Data Custodians");
+        clickByClass("data-custodian");
+        clickByName("next");
+    }
+
+    @When("^I log into Data Custodian$")
+    public void I_log_into_Data_Custodian() throws Throwable {
+        fillInByName("j_username", StepUtils.USERNAME);
+        fillInByName("j_password", StepUtils.PASSWORD);
+        clickByName("submit");
+    }
+
+    @When("^I select Scopes$")
+    public void I_select_Scope() throws Throwable {
+        clickByClass("scope");
+        clickByName("next");
+    }
+
+    @When("^I log into Third Party$")
+    public void I_log_into_Third_Party() throws Throwable {
+        fillInByName("j_username", StepUtils.USERNAME);
+        fillInByName("j_password", StepUtils.PASSWORD);
+        clickByName("submit");
+    }
+
+    @When("^I log in as a Data Custodian$")
+    public void I_login_as_a_Data_Custodian() throws Throwable {
+        StepUtils.login(StepUtils.DATA_CUSTODIAN_CONTEXT, StepUtils.CUSTODIAN_USERNAME, StepUtils.CUSTODIAN_PASSWORD);
+    }
+
+    @When("^I select Usage Point$")
+    public void I_select_Usage_Point() throws Throwable {
+        WebElement usagePointLink = driver.findElement(By.linkText("Front Electric Meter"));
+        usagePointLink.click();
+    }
+
+    @When("^I select Meter Reading$")
+    public void I_select_Meter_Reading() throws Throwable {
+        WebElement usagePointLink = driver.findElement(By.linkText("Fifteen Minute Electricity Consumption"));
+        usagePointLink.click();
     }
 
     @And("^I enter my username and password$")
@@ -182,36 +165,34 @@ public class RetailCustomersSteps {
         clickByName("deny");
     }
 
+    @And("^I select a Third Party from the list$")
+    public void I_select_a_Third_Party_from_the_list() throws Throwable {
+        clickLinkByText("Third Parties");
+        clickByClass("third-party");
+        clickByName("next");
+    }
+
+    @Then("^I should be taken to the Third Party login page$")
+    public void I_should_be_taken_to_the_Third_Party_login_page() throws Throwable {
+        assertNotNull("Login field missing", driver.findElement(By.name("j_username")));
+        assertNotNull("Password field missing", driver.findElement(By.name("j_password")));
+    }
+
+    @Then("^I should see Usage Point with title \"([^\"]*)\"$")
+    public void I_should_see_my_Usage_Points_with_title(String title) throws Throwable {
+        assertTrue(driver.getPageSource().contains(title));
+    }
+
     @Then("^I should be redirected to the home page$")
     public void I_should_be_redirected_to_the_home_page() throws Throwable {
         assertTrue(driver.getCurrentUrl().contains("RetailCustomer"));
         assertTrue(driver.getCurrentUrl().endsWith("/home"));
     }
 
-    @When("^I select a Data Custodian from the list$")
-    public void I_select_a_Data_Custodian_from_the_list() throws Throwable {
-        clickLinkByText("Data Custodians");
-        clickByClass("data-custodian");
-        clickByName("next");
-    }
-
-    @When("^I log into Data Custodian$")
-    public void I_log_into_Data_Custodian() throws Throwable {
-        fillInByName("j_username", StepUtils.USERNAME);
-        fillInByName("j_password", StepUtils.PASSWORD);
-        clickByName("submit");
-    }
-
     @Then("^I should see Scope selection screen$")
     public void I_should_see_Scope_selection_screen() throws Throwable {
         assertContains("Select Scope");
         assertContains("FB_4_5_15_IntervalDuration_3600_BlockDuration_monthly_HistoryLength_13");
-    }
-
-    @When("^I select Scopes$")
-    public void I_select_Scope() throws Throwable {
-        clickByClass("scope");
-        clickByName("next");
     }
 
     @Then("^I should see authorization screen$")
@@ -225,40 +206,36 @@ public class RetailCustomersSteps {
         assertContains("ConEdison");
     }
 
-    @And("^I select a Third Party from the list$")
-    public void I_select_a_Third_Party_from_the_list() throws Throwable {
-        clickLinkByText("Third Parties");
-        clickByClass("third-party");
-        clickByName("next");
+    @Then("^I should see Meter Reading Details$")
+    public void I_should_see_Meter_Reading_Details() throws Throwable {
+        assertTrue("MeterReading title missing", driver.getPageSource().contains("Fifteen Minute Electricity Consumption"));
+        assertTrue("ReadingType title missing", driver.getPageSource().contains("Type of Meter Reading Data"));
+        assertTrue("Interval blocks missing", driver.getPageSource().contains("86400"));
+        assertTrue(driver.getPageSource().contains("974"));
+        assertTrue(driver.getPageSource().contains("900"));
+        assertTrue(driver.getPageSource().contains("965"));
+
+        WebElement element = driver.findElement(By.cssSelector(".reading-qualities"));
+
+        assertThat(element.getText(), containsString("8"));
     }
 
-    @When("^I log into Third Party$")
-    public void I_log_into_Third_Party() throws Throwable {
-        fillInByName("j_username", StepUtils.USERNAME);
-        fillInByName("j_password", StepUtils.PASSWORD);
-        clickByName("submit");
-    }
-
-    @When("^I log in as a Data Custodian$")
-    public void I_login_as_a_Data_Custodian() throws Throwable {
-        StepUtils.login(StepUtils.DATA_CUSTODIAN_CONTEXT, StepUtils.CUSTODIAN_USERNAME, StepUtils.CUSTODIAN_PASSWORD);
-    }
-
-    @Given("^a Retail Customer with Usage Points$")
-    public void a_Retail_Customer_with_Usage_Points() throws Throwable {
-        CucumberSession.setUsername(StepUtils.newUsername());
-
-        StepUtils.registerUser(CucumberSession.getUsername(), StepUtils.newFirstName(), StepUtils.newLastName(), StepUtils.PASSWORD);
-        CucumberSession.setUUID(UUID.fromString("48C2A019-5598-4E16-B0F9-49E4FF27F5FB"));
-        StepUtils.addUsagePoint(CucumberSession.getUsername(), CucumberSession.getUUID().toString());
-        StepUtils.importUsagePoint();
-        StepUtils.logout();
-        StepUtils.login(StepUtils.THIRD_PARTY_CONTEXT, "alan", StepUtils.PASSWORD);
-    }
-
-    @Then("^I should see Local Time Parameters$")
-    public void I_should_see_Local_Time_Parameters() throws Throwable {
+    @Then("^I should see Usage Point details$")
+    public void I_should_see_Usage_Point_details() throws Throwable {
+        assertTrue(driver.getPageSource().contains("Usage Summary"));
+        assertTrue(driver.getPageSource().contains("Quality Summary"));
         assertContains("Local time zone offset from UTCTime");
         assertContains("-18000");
+    }
+
+    @Then("^I should be logged in$")
+    public void I_should_be_logged_in() throws Throwable {
+        assertTrue(driver.getPageSource().contains("logout"));
+        assertFalse(driver.getPageSource().contains("login"));
+    }
+
+    @Then("^I should see the login form$")
+    public void I_should_see_the_login_form() throws Throwable {
+        assertTrue(driver.getPageSource().contains("Login"));
     }
 }
--- src/test/resources/features/RetailCustomers.feature
+++ src/test/resources/features/RetailCustomers.feature
@@ -4,37 +4,31 @@ Feature: Retail Customers
   So that I can see my UsagePoints
 
   Scenario: Retail Customer authorizes Usage Points access
-    Given I have a Retail Customer account
+    Given a Retail Customer with Usage Points
 
-    When I log in as Alan Turing into Third Party
-    And I look at my Usage Points page
-    And I enter my username and password
+    When I look at my Usage Points page
     And I authorize Third Party
+
     Then I should see Usage Point with title "Front Electric Meter"
 
   Scenario: Retail Customer views Usage Points with Meter Readings
     Given a Retail Customer with Usage Points
 
     When I look at my Usage Points page
+    And I authorize Third Party
     And I select Usage Point
-    Then I should see Electric Power Usage Summary
-    And I should see Electric Power Quality Summary
-    And I should see Local Time Parameters
+    Then I should see Usage Point details
 
     When I select Meter Reading
-    Then I should see Meter Reading
-    And I should see Reading Type
-    And I should see Interval Blocks
-    And I should see Interval Readings
-    And I should see Reading Qualities
+    Then I should see Meter Reading Details
 
   Scenario: Retail Customer denies Usage Points access
     Given I have a Retail Customer account
 
     When I log in as Alan Turing into Third Party
     And I look at my Usage Points page
-    And I enter my username and password
     And I deny Third Party
+
     Then I should be redirected to the home page
 
   Scenario: Retail Customer authorizes Scope
@@ -97,10 +91,10 @@ Feature: Retail Customers
     When I deny Third Party
     Then I should be redirected to the home page
 
-  Scenario: Retail Customer views Usage Points with Meter Readings
-    Given I have a Retail Customer account on the DataCustodian and the ThirdParty
-    And I have energy usage information on the DataCustodian
-
-    When I log in to the Third Party
-    And I look at my energy usage information
-    Then I should see my energy usage information
+#  Scenario: Retail Customer views Usage Points with Meter Readings
+#    Given I have a Retail Customer account on the DataCustodian and the ThirdParty
+#    And I have energy usage information on the DataCustodian
+#
+#    When I log in to the Third Party
+#    And I look at my energy usage information
+#    Then I should see my energy usage information
