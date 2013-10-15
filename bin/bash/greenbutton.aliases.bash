4b7f43a5919f28ec550d3cd6d20784347a64e60c
Ian Zabel and Josh Clark <pair+izabel+jclark@pivotallabs.com>
2013-10-15 15:57:20 -0400

    Adds eachsub and fetch-all utilities to bash aliases

--- bin/bash/greenbutton.aliases.bash
+++ bin/bash/greenbutton.aliases.bash
@@ -9,3 +9,12 @@ about-alias 'general aliases'
 alias gb="cd ~/workspace/OpenESPI-Greenbutton-java"
 alias gbdc="cd ~/workspace/OpenESPI-Greenbutton-java/OpenESPI-DataCustodian-java"
 alias gbtp="cd ~/workspace/OpenESPI-Greenbutton-java/OpenESPI-ThirdParty-java"
+alias eachsub="git submodule foreach"
+
+function fetch-all {
+  gb
+  git co master
+  git pull --rebase
+  git submodule foreach git co master
+  git submodule foreach git pull --rebase
+}
