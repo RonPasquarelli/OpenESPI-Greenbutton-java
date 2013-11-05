bf9eceb3ea5fbfd0affcb8ad4646fdd566704a69
Brian Kelly and Ian Zabel <pair+bkelly+ian@pivotallabs.com>
2013-11-05 11:26:04 -0500

    Display OAuth token on the authorization screen.
    [Finishes #60087868]

--- src/test/java/features/steps/SubscriptionSteps.java
+++ src/test/java/features/steps/SubscriptionSteps.java
@@ -1,12 +1,17 @@
 package features.steps;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import static features.steps.StepUtils.*;
 import static org.custommonkey.xmlunit.XMLAssert.assertXpathExists;
+import static org.hamcrest.Matchers.isEmptyString;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
 
 public class SubscriptionSteps {
     private WebDriver driver = WebDriverSingleton.getInstance();
@@ -37,5 +42,17 @@ public class SubscriptionSteps {
     @Then("^I should receive XML for that Subscription$")
     public void I_should_receive_XML_for_that_Subscription() throws Throwable {
         assertXpathExists("/:feed", driver.getPageSource());
+    }
+
+    @And("^I navigate to the Authorizations page$")
+    public void I_navigate_to_the_Authorizations_page() throws Throwable {
+        clickLinkByText("Authorizations");
+    }
+
+    @Then("^I should see a list of Authorizations$")
+    public void I_should_see_a_list_of_Authorizations() throws Throwable {
+        assertContains("Authorizations");
+        assertThat(driver.findElement(By.cssSelector("#authorizations tr td.access_token")).getText(), not(isEmptyString()));
+
     }
 }
--- src/test/resources/features/Subscription.feature
+++ src/test/resources/features/Subscription.feature
@@ -9,3 +9,10 @@ Feature: Subscription
     When I log in as Alan Turing
     And I request a Subscription using the REST API
     Then I should receive XML for that Subscription
+
+  Scenario: Third Party customer accesses Authorizations page
+    Given an authorized Third Party
+
+    When I log in as Alan Turing
+    And I navigate to the Authorizations page
+    Then I should see a list of Authorizations
