db4a32f5700a977580d8e1fd3d16edc997d7a766
Brian Kelly <pair+bkelly@pivotallabs.com>
2013-11-12 14:13:38 -0500

    Adds `morememory`, `gbreload` and `gogogo` aliases.

--- bin/bash/greenbutton.aliases.bash
+++ bin/bash/greenbutton.aliases.bash
@@ -6,6 +6,10 @@
 cite about-alias
 about-alias 'general aliases'
 
+alias gbreload="bash-it disable alias greenbutton; bash-it enable alias greenbutton"
+alias morememory="export MAVEN_OPTS='-Xmx512m -XX:MaxPermSize=256m'"
+alias gogogo="gbc && mvn clean install && gbdc && mvn clean install && gbtp && mvn clean install && gb mvn clean install"
+
 alias gb="cd ~/workspace/OpenESPI-Greenbutton-java"
 alias gbdc="cd ~/workspace/OpenESPI-Greenbutton-java/OpenESPI-DataCustodian-java"
 alias gbtp="cd ~/workspace/OpenESPI-Greenbutton-java/OpenESPI-ThirdParty-java"
